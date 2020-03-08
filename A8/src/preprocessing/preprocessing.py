fc4e1880483adc107a0212ce527bb0582f8bcf87
Abhijit Mali <abhijitmali@hotmail.com>
2020-03-08 15:22:56 +0530

    Achieved test accuracy 91.53%
    TRACC = 91.53%, TSACC = 91.53%

--- A8/src/preprocessing/preprocessing.py
+++ A8/src/preprocessing/preprocessing.py
@@ -9,11 +9,12 @@ class Preprocessing(object):
         return transforms.Compose([
             transforms.Pad(padding=1, padding_mode="edge"),
             transforms.RandomHorizontalFlip(),  # randomly flip and rotate
-            transforms.RandomRotation(10),
+            transforms.RandomRotation(20),
+            # transforms.RandomErasing(),
+            transforms.RandomCrop(size=(32, 32), padding=4),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-
 
     def gettesttransforms(self):
         # Test Phase transformations
