018651d6c880ccb0b5001dc5418f981b91fa6677
Abhijit Mali <abhijitmali@hotmail.com>
2020-04-12 03:14:27 +0530

    achieved 90% accuracy
    achieved 90% accuracy

--- A11/src/preprocessing/pytorchtransforms.py
+++ A11/src/preprocessing/pytorchtransforms.py
@@ -9,12 +9,10 @@ class PytorchTransforms(object):
             transforms.Pad(padding=1, padding_mode="edge"),
             transforms.RandomHorizontalFlip(),  # randomly flip and rotate
             transforms.RandomRotation(20),
-            # transforms.RandomErasing(),
             transforms.RandomCrop(size=(32, 32), padding=4),
             transforms.ToTensor(),
             transforms.Normalize(mean, std),
             transforms.RandomErasing(scale=(0.1, 0.1), ratio=(1, 1)),
-            # Cutout(1, 8)
         ])
 
     def gettesttransforms(self, mean, std):
